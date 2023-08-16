a74525c7dc5f489edf9656e6f914bdc1b66c2fad
Luna <toniang1983@gmail.com>
2023-08-16 19:43:27 +0700

    Almost Complete for level 6

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
 public class GameOver : MonoBehaviour
 {
     [Header("StartPos")]
     public Vector3 startpos;
+
+    [Header("StartEvent")]
+    public TextMeshProUGUI Text;
+    public float timer;
+    private float time;
+    private bool start;
+
+    private void Update()
+    {     
+        if(start)
+        {
+            Text.text = time.ToString("0");
+            time -= 1 * Time.deltaTime;
+            if (time <= 0)
+            {
+                Endgame();
+            }
+        }
+        else
+        {
+            time = timer;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("GameOver"))
+        if (collision.gameObject.name == "EndGame")
         {
-            gameObject.transform.position = new Vector3(startpos.x, startpos.y, startpos.z);
+            Endgame();
         }
+        if (collision.gameObject.name == "StartGame")
+        {
+            start = true;
+            Text.gameObject.SetActive(true);
+        }
+    }
+    void Endgame()
+    {
+        gameObject.transform.position = new Vector3(startpos.x, startpos.y, startpos.z);
+        start = false;
+        Text.gameObject.SetActive(false);
     }
 }
