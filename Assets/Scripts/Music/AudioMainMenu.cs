99c6c5a630ce47b6b23b2415ab8e0dfeef732d2e
Luna <toniang1983@gmail.com>
2023-08-24 16:06:08 +0700

    Fixed Music

--- Assets/Scripts/Music/AudioMainMenu.cs
+++ Assets/Scripts/Music/AudioMainMenu.cs
@@ -1,24 +1,36 @@
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AudioMainMenu : MonoBehaviour
 {
     public AudioSource Music;
+    private static AudioMainMenu instance;
+
     private void Awake()
     {
-        DontDestroyOnLoad(this);
-    }
-    private void Start()
-    {
-        Music.Play();
-    }
-    public void Update()
-    {
-        if (SceneManager.GetActiveScene().buildIndex >= 2)
+        // Implement Singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
         {
             Destroy(gameObject);
         }
     }
 
+    private void Start()
+    {
+        Music.Play();
+    }
+
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= 2)
+        {
+            // Destroy the object if it's not the singleton instanc
+            Destroy(gameObject);
+        }
+    }
 }
